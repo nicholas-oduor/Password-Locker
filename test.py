4faa24b40fdabb92f8935600940c49e87bcd113b
nicholas-oduor <oduor5742@gmail.com>
2021-02-14 14:56:16 +0300

    added test_init method

--- test.py
+++ test.py
@@ -13,3 +13,12 @@ class TestUser(unittest.TestCase):
         '''
         
         self.new_user = User('nicholas', '123456')
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"nicholas")
+        self.assertEqual(self.new_user.password,"123456")
+        
